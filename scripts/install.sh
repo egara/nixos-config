7c048f4c95d19197becdceba3df8a94afd41aafe
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2023-07-01 08:58:26 +0200

    Updating installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -46,7 +46,7 @@ echo "         NixOS will be re-installed"
 echo "         This is a destructive operation"
 echo
 read -p "Are you sure? [y/N]" -n 1 -r
-echo
+
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   sudo true
 
