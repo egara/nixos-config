22582b064f03c762ab4cf407ad9475c13d4b45f3
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2023-07-03 20:16:33 +0200

    Including label creation for swap partition

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Script for installing NixOS
+# ---------------------------
+#
+# @author: Eloy García Almadén
+# @email: eloy.garcia.pca@gmail.com
+# GitHub: https://github.com/egara/nixos-config
+# Inspired by Martin Wimpress: https://github.com/wimpysworld/nix-config
+# ---------------------------
+
 set -euo pipefail
 
 TARGET_HOST="${1:-}"
@@ -35,50 +44,32 @@ fi
 #   exit 1
 # fi
 
-# # Check if the machine we're provisioning expects a keyfile to unlock a disk.
-# # If it does, generate a new key, and write to a known location.
-# if grep -q "data.keyfile" "nixos/$TARGET_HOST/disks.nix"; then
-#   echo -n "$(head -c32 /dev/random | base64)" > /tmp/data.keyfile
-# fi
+sudo true
 
-echo "WARNING! The disks in $TARGET_HOST are about to get wiped"
-echo "         NixOS will be re-installed"
-echo "         This is a destructive operation"
-echo
-read -p "Are you sure? [y/N]" -n 1 -r
+#sudo nix run github:nix-community/disko \
+#  --extra-experimental-features "nix-command flakes" \
+#  --no-write-lock-file \
+#  -- \
+#  --mode zap_create_mount \
+#  "nixos/$TARGET_HOST/disks.nix"
 
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sudo true
+sudo nix run github:nix-community/disko \
+  --extra-experimental-features "nix-command flakes" \
+  --no-write-lock-file \
+  -- \
+  --mode zap_create_mount \
+  "hosts/disko-config.nix"
 
-  # sudo nix run github:nix-community/disko \
-  #   --extra-experimental-features "nix-command flakes" \
-  #   --no-write-lock-file \
-  #   -- \
-  #   --mode zap_create_mount \
-  #   "nixos/$TARGET_HOST/disks.nix"
+# Labeling swap
+sudo mkswap -L /dev/vda2
 
-  sudo nix run github:nix-community/disko \
-    --extra-experimental-features "nix-command flakes" \
-    --no-write-lock-file \
-    -- \
-    --mode zap_create_mount \
-    "hosts/disko-config.nix"
+# Creating mounting point for BTRFS full volume
+sudo mkdir -p /mnt/mnt/defvol
 
-  # Creating mounting point for BTRFS full volume
-  sudo mkdir -p /mnt/mnt/defvol
+sudo nixos-install --no-root-password --flake ".#$TARGET_HOST"
 
-  sudo nixos-install --no-root-password --flake ".#$TARGET_HOST"
-
-  # Rsync nix-config to the target install and set the remote origin to SSH.
-  rsync -a --delete "$HOME/Zero/" "/mnt/home/$TARGET_USER/Zero/"
-  pushd "/mnt/home/$TARGET_USER/Zero/nixos-config"
-  git remote set-url origin git@github.com:egara/nixos-config.git
-  popd
-
-  # # If there is a keyfile for a data disk, put copy it to the root partition and
-  # # ensure the permissions are set appropriately.
-  # if [[ -f "/tmp/data.keyfile" ]]; then
-  #   sudo cp /tmp/data.keyfile /mnt/etc/data.keyfile
-  #   sudo chmod 0400 /mnt/etc/data.keyfile
-  # fi
-fi
+# Rsync nix-config to the target install and set the remote origin to SSH.
+rsync -a --delete "$HOME/Zero/" "/mnt/home/$TARGET_USER/Zero/"
+pushd "/mnt/home/$TARGET_USER/Zero/nixos-config"
+git remote set-url origin git@github.com:egara/nixos-config.git
+popd
