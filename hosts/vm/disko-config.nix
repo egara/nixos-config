bf6002e33811dcea36965dcead8b866c015043da
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2023-09-12 19:10:36 +0200

    Fixing disko efi partition

--- hosts/vm/disko-config.nix
+++ hosts/vm/disko-config.nix
@@ -3,7 +3,8 @@
     disk = {
       vdb = {
         type = "disk";
-        device = "/dev/disk/by-diskseq/1";
+        #device = "/dev/disk/by-diskseq/1";
+        device = "/dev/vda";
         content = {
           type = "gpt";
           partitions = {
