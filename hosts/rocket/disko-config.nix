1c0e35fb58fdf8569ceaadd07440d6eefcda1456
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2023-09-12 20:02:30 +0200

    Adapting disko configuration to rocker host

--- hosts/rocket/disko-config.nix
+++ hosts/rocket/disko-config.nix
@@ -5,51 +5,23 @@
         type = "disk";
         device = builtins.elemAt disks 0;
         content = {
-          type = "table";
-          format = "gpt";
-          partitions = [
-            # UEFI
-            {
-              name = "UEFI";
-              start = "1MiB";
-              end = "513MiB";
-              fs-type = "fat32";
-              bootable = true;
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot/efi";
-              };
-            }
- 
-            # SWAP
-            {
-              name = "swap";
-              start = "513MiB";
-              end = "8537MiB";
-              part-type = "primary";
-              fs-type = "linux-swap";
-              content = {
-                type = "swap";
-                randomEncryption = true;
-              };
-            }
-
-            # SYSTEM
-            {
-              name = "system";
-              start = "8537MiB";
-              end = "100%";
+          type = "gpt";
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            root = {
+              end = "-8G";
               content = {
                 type = "btrfs";
-
-                # Override existing partition and set a label called system
-                extraArgs = [ "-f --label system" ];
-
+                extraArgs = [ "-f --label system" ]; # Override existing partition and set a label called system
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
                 subvolumes = {
                   "@" = {
-                    mountpoint = "/";
                     mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/";
                   };
                   "@home" = {
                     mountpoint = "/home";
@@ -59,8 +31,16 @@
                   };
                 };
               };
-            }
-          ];
+            };
+            swap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                resumeDevice = true; # resume from hiberation from this device
+              };
+            };
+          };
         };
       };
     };
