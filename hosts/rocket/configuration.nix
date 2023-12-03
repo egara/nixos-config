0d341b1264c260d95a459ce7133ed19d110a6c7b
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2023-12-03 09:17:43 +0100

    Enabling wayland session by default and amd graphics on rocket

--- hosts/rocket/configuration.nix
+++ hosts/rocket/configuration.nix
@@ -6,5 +6,26 @@
   ##########################################
 
   networking.hostName = "rocket"; # Define your hostname.
-  
+
+  # Enabling AMD-ATI driver
+  boot = {
+    initrd = {
+      kernelModules = [ "amdgpu" ];
+    };
+  };
+
+  # Enabling amdgpu driver on XServer
+  services = {
+    xserver = {
+      videoDrivers = [ "amdgpu" ];
+      displayManager = {
+        defaultSession = "plasmawayland";
+        sddm = {
+          wayland = {
+            enable = true;
+          };
+        };
+      };
+    };
+  };
 }
