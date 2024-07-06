131a9183c5cd9a5c0df13b889a205a3ae8d97de6
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2024-07-06 15:34:16 +0200

    Trying again. Fixing an old kernel which should work

--- hosts/ironman/desktop/hyprland/configuration.nix
+++ hosts/ironman/desktop/hyprland/configuration.nix
@@ -8,6 +8,8 @@
 
   networking.hostName = "ironman"; # Define your hostname.
 
+  boot.kernelPackages = pkgs.linuxPackages_5_10;
+
 #  # Enabling NVIDIA driver
 #  boot = {
 #    initrd = {
@@ -16,81 +18,81 @@
 #  };
 
   # Kernel parameters passed in GRUB
-  boot.kernelParams = [ 
-    "i915.enable_rc6=0" 
-    "pcie_port_pm=off" 
-    "acpi_osi=\"!Windows 2015\""
-  ];
+  # boot.kernelParams = [ 
+  #   "i915.enable_rc6=0" 
+  #   "pcie_port_pm=off" 
+  #   "acpi_osi=\"!Windows 2015\""
+  # ];
 
-  hardware.graphics = {
-    enable = true;
-    extraPackages = with pkgs; [
-      vpl-gpu-rt # or intel-media-sdk for QSV
-    ];
+  # hardware.graphics = {
+  #   enable = true;
+  #   extraPackages = with pkgs; [
+  #     vpl-gpu-rt # or intel-media-sdk for QSV
+  #   ];
+  # };
+
+  # Hybrid grafics configuration
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+  # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
-  # # Hybrid grafics configuration
-  # hardware.nvidia = {
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
 
-  #   # Modesetting is required.
-  #   modesetting.enable = true;
-
-  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-  #   # Enable this if you have graphical corruption issues or application crashes after waking
-  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-  #   # of just the bare essentials.
-  #   powerManagement.enable = false;
-
-  #   # Fine-grained power management. Turns off GPU when not in use.
-  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-  #   powerManagement.finegrained = false;
-
-  #   # Use the NVidia open source kernel module (not to be confused with the
-  #   # independent third-party "nouveau" open source driver).
-  #   # Support is limited to the Turing and later architectures. Full list of 
-  #   # supported GPUs is at: 
-  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-  #   # Only available from driver 515.43.04+
-  #   # Currently alpha-quality/buggy, so false is currently the recommended setting.
-  #   open = false;
-
-  #   # Enable the Nvidia settings menu,
-  # # accessible via `nvidia-settings`.
-  #   nvidiaSettings = true;
-
-  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
-
-  # hardware.nvidia.prime = {
-  #   offload = {
-  #     enable = true;
-  #     enableOffloadCmd = true;
-  #   };
-
-  #   # integrated
-  #   intelBusId = "PCI:0:2:0";
+    # integrated
+    intelBusId = "PCI:0:2:0";
         
-  #   # dedicated
-  #   nvidiaBusId = "PCI:1:0:0";
-  # };
+    # dedicated
+    nvidiaBusId = "PCI:1:0:0";
+  };
 
-  boot.extraModprobeConfig = ''
-    blacklist nouveau
-    options nouveau modeset=0
-  '';
+  # boot.extraModprobeConfig = ''
+  #   blacklist nouveau
+  #   options nouveau modeset=0
+  # '';
     
-  services.udev.extraRules = ''
-    # Remove NVIDIA USB xHCI Host Controller devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA USB Type-C UCSI devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA Audio devices, if present
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
-    # Remove NVIDIA VGA/3D controller devices
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
-  '';
-  boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
+  # services.udev.extraRules = ''
+  #   # Remove NVIDIA USB xHCI Host Controller devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c0330", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA USB Type-C UCSI devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x0c8000", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA Audio devices, if present
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x040300", ATTR{power/control}="auto", ATTR{remove}="1"
+  #   # Remove NVIDIA VGA/3D controller devices
+  #   ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x10de", ATTR{class}=="0x03[0-9]*", ATTR{power/control}="auto", ATTR{remove}="1"
+  # '';
+  # boot.blacklistedKernelModules = [ "nouveau" "nvidia" "nvidia_drm" "nvidia_modeset" ];
 
   # SDDM
   services = {
@@ -105,7 +107,7 @@
 
     xserver = {
       enable = true;
-      videoDrivers = [ "intel" ];
+      videoDrivers = [ "nvidia" ];
 
       xkb = {
         layout = "es";
