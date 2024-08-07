8b1625eac7bae98f33a65e656de1a4f01537d91e
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2024-08-07 09:56:34 +0200

    A new script tweaked for enabling again waybar if crashed when lid is opened or closed

--- home-manager/hyprland/scripts/disable-laptop-screen.sh
+++ home-manager/hyprland/scripts/disable-laptop-screen.sh
@@ -7,18 +7,31 @@
 # @email: eloy.garcia.pca@gmail.com
 # ------------------------------------------
 
-# Run hyprctl monitors command and store the output
+# Running hyprctl monitors command and store the output
 output=$(hyprctl monitors)
 
-# Check if the output contains "HDMI-"
+# Checking if the output contains "HDMI-"
 if [[ "$output" == *"HDMI-"* ]]; then
   if [[ $1 == "open" ]]; then
     hyprctl keyword monitor "eDP-1,1920x1080,1920x0,1"
-    echo 'Lip is opened. Enabling laptop screen' | systemd-cat -p info
+    echo "Lip is opened. Enabling laptop screen" | systemd-cat -p info
   else
     hyprctl keyword monitor "eDP-1,disable"
-    echo 'Lip is closed. Disabling laptop screen' | systemd-cat -p info
+    echo "Lip is closed. Disabling laptop screen" | systemd-cat -p info
   fi
+
+  # Checking if waybar has crashed
+  echo "Checking if waybar has crashed" | systemd-cat -p info
+  sleep 3
+  waybarActive=$(ps -A | grep waybar | wc -l)
+
+  if [ "$waybarActive" -gt 0 ]
+  then 
+    echo "waybar is OK. waybarActive = $waybarActive. Skipping..." | systemd-cat -p info
+  else
+    echo "waybar has crashed. waybarActive = $waybarActive. Enabling it again" | systemd-cat -p info
+    waybar &
+  fi  
 else
-    echo "no!"
+    echo "No external monitor HDMI-* has been detected. Type hyprctl monitors to see active monitors and modify this script." | systemd-cat -p info
 fi
