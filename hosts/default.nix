e80dc8ddc6f4eec03525d040d44fc44c434d7012
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2025-08-12 12:07:11 +0200

    Refactoring desktop modules and home-manager modules which will be imported depending on the desktop environment selected

--- hosts/default.nix
+++ hosts/default.nix
@@ -77,198 +77,176 @@ in
   };
 
   # Rocket profile (for testing purposes)
-  # Hyprland configurations by default
-  # Comment Hyprland lines and uncomment Plasma 6 lines in order to switch installations
-  rocket = lib.nixosSystem {
-
-    inherit system;
-    
-    specialArgs = {
-      inherit inputs username location pkgs-stable;
-      host = {
+  rocket =
+    let
+      hostArg = {
         hostName = "rocket";
+        desktop = "plasma"; # "hyprland" or "plasma"
       };
-    };
+    in
+    lib.nixosSystem {
+      inherit system;
 
-    modules = [
-      # Execute disko module
-      disko.nixosModules.disko {
-        _module.args.disks = [ "/dev/sda" ];
-        imports = [(import ./rocket/disko-config.nix)];
-      }
+      specialArgs = {
+        inherit inputs username location pkgs-stable;
+        host = hostArg;
+      };
 
-      # Execute hardware configuration module
-      ./rocket/hardware-configuration.nix
+      modules = [
+        # Execute disko module
+        disko.nixosModules.disko {
+          _module.args.disks = [ "/dev/sda" ];
+          imports = [(import ./rocket/disko-config.nix)];
+        }
 
-      # Execute common configuration for BIOS legacy systems
-      ./bios-configuration.nix
-      
-      # Execute common configuration module
-      ./configuration.nix
+        # Execute hardware configuration module
+        ./rocket/hardware-configuration.nix
 
-      # Execute common configuration module for rocket
-      ./rocket/configuration.nix
+        # Execute common configuration for BIOS legacy systems
+        ./bios-configuration.nix
 
-      ########################
-      # Hyprland Configuration
-      ########################
-      # Execute specific configuration module for this profile
-      #./rocket/desktop/hyprland/configuration.nix
-      # Execute configuration module for hyprland
-      #../modules/desktop/hyprland.nix
- 
-      ########################
-      # Plasma 6 Configuration
-      ########################
-      # Execute specific configuration module for this profile
-      ./rocket/desktop/plasma/configuration.nix
-      # Execute configuration module for plasma
-      ../modules/desktop/plasma.nix
+        # Execute common configuration module
+        ./configuration.nix
 
-#      # Stylix configuration module
-#      stylix.nixosModules.stylix
+        # Execute common configuration module for rocket
+        ./rocket/configuration.nix
 
-      # Execute home manager module
-      home-manager.nixosModules.home-manager {
-        home-manager.backupFileExtension = "backup";
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = {
-          inherit username;
-          host = {
-            hostName = "rocket";
+        # Desktop Environment modules
+        ({ config, lib, host, ... }: {
+          imports =
+            lib.optionals (host.desktop == "hyprland") [
+              ./rocket/desktop/hyprland/configuration.nix
+              ../modules/desktop/hyprland.nix
+            ] ++ lib.optionals (host.desktop == "plasma") [
+              ./rocket/desktop/plasma/configuration.nix
+              ../modules/desktop/plasma.nix
+            ];
+        })
+
+        #      # Stylix configuration module
+        #      stylix.nixosModules.stylix
+
+        # Execute home manager module
+        home-manager.nixosModules.home-manager {
+          home-manager.backupFileExtension = "backup";
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {
+            # inherit username hyprland-plugins;
+            inherit username;
+            host = hostArg;
           };
-        };
-        home-manager.users.${username} = {
-          ########################
-          # Hyprland Configuration
-          ########################
-          # (default)
-          #imports = [(import ./home.nix)] ++ [(import ../home-manager/desktop/hyprland/home.nix)];
-
-          ########################
-          # Plasma 6 Configuration
-          ########################
-          imports = [(import ./home.nix)];
-        };
-      }
-
-    ];
-
-  };
+          home-manager.users.${username} = {
+            imports = [ (import ./home.nix) ] ++ lib.optionals (hostArg.desktop == "hyprland") [
+              (import ../home-manager/desktop/hyprland/home.nix)
+            ];
+          };
+        }
+      ];
+    };
 
   # Ironman profile
-  # Hyprland configurations by default
-  # Comment Hyprland lines and uncomment Plasma 6 lines in order to switch installations
-  ironman = lib.nixosSystem {
-
-    inherit system;
-    
-    specialArgs = {
-      inherit inputs username location pkgs-stable;
-      host = {
+  ironman =
+    let
+      hostArg = {
         hostName = "ironman";
+        desktop = "hyprland"; # "hyprland" or "plasma"
       };
+    in
+    lib.nixosSystem {
+      inherit system;
+
+      specialArgs = {
+        inherit inputs username location pkgs-stable;
+        host = hostArg;
+      };
+
+      modules = [
+        # Execute disko module
+        disko.nixosModules.disko {
+          # module configuration
+          _module.args.disks = [ "/dev/sda" ];
+          imports = [(import ./ironman/disko-config.nix)];
+        }
+
+        # Execute hardware configuration module
+        ./ironman/hardware-configuration.nix
+
+        # Execute common configuration for EFI boot systems
+        ./efi-configuration.nix
+
+        # Execute common configuration module
+        ./configuration.nix
+
+        # Execute common configuration module for ironman
+        ./ironman/configuration.nix
+
+        # Autofirma module (it is a module itself!)
+        autofirma-nix.nixosModules.default
+
+        ({ config, pkgs, ... }: {
+          # The autofirma command becomes available system-wide
+          programs.autofirma = {
+            enable = true;
+            firefoxIntegration.enable = true;
+          };
+
+          # # DNIeRemote integration for using phone as NFC reader
+          # programs.dnieremote = {
+          #   enable = true;
+          # };
+
+          # The FNMT certificate configurator
+          programs.configuradorfnmt = {
+            enable = true;
+            firefoxIntegration.enable = true;
+          };
+
+          # Firefox configured to work with AutoFirma
+          programs.firefox = {
+            enable = true;
+            policies.SecurityDevices = {
+              "OpenSC PKCS#11" = "${pkgs.opensc}/lib/opensc-pkcs11.so";
+              "DNIeRemote" = "${config.programs.dnieremote.finalPackage}/lib/libdnieremotepkcs11.so";
+            };
+          };
+
+          # Enable PC/SC smart card service
+          services.pcscd.enable = true;
+        })
+
+        # Desktop Environment module (this is a module itself!)
+        ({ config, lib, host, ... }: {
+          # Depending on the desktop, some modules are imported
+          imports =
+            lib.optionals (host.desktop == "hyprland") [
+              ./ironman/desktop/hyprland/configuration.nix
+              ../modules/desktop/hyprland.nix
+            ] ++ lib.optionals (host.desktop == "plasma") [
+              ./ironman/desktop/plasma/configuration.nix
+              ../modules/desktop/plasma.nix
+            ];
+        })
+
+        # Execute home manager module
+        home-manager.nixosModules.home-manager {
+          # Module configuration
+          home-manager.backupFileExtension = "backup";
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = {
+            # inherit username hyprland-plugins;
+            inherit username;
+            host = hostArg;
+          };
+          home-manager.users.${username} = {
+            imports = [ (import ./home.nix) ] ++ lib.optionals (hostArg.desktop == "hyprland") [
+              (import ../home-manager/desktop/hyprland/home.nix)
+            ];
+          };
+        }
+      ];
     };
-
-    modules = [
-      # Execute disko module
-      disko.nixosModules.disko {
-        _module.args.disks = [ "/dev/sda" ];
-        imports = [(import ./ironman/disko-config.nix)];
-      }
-
-      # Execute hardware configuration module
-      ./ironman/hardware-configuration.nix
-
-      # Execute common configuration for EFI boot systems
-      ./efi-configuration.nix
-      
-      # Execute common configuration module
-      ./configuration.nix
-
-      # Execute common configuration module for ironman
-      ./ironman/configuration.nix
-
-      # Autofirma module
-      autofirma-nix.nixosModules.default
-      
-      ({ config, pkgs, ... }: {
-        # The autofirma command becomes available system-wide
-        programs.autofirma = {
-          enable = true;
-          firefoxIntegration.enable = true;
-        };
-
-        # # DNIeRemote integration for using phone as NFC reader
-        # programs.dnieremote = {
-        #   enable = true;
-        # };
-
-        # The FNMT certificate configurator
-        programs.configuradorfnmt = {
-          enable = true;
-          firefoxIntegration.enable = true;
-        };
-
-        # Firefox configured to work with AutoFirma
-        programs.firefox = {
-          enable = true;
-          policies.SecurityDevices = {
-            "OpenSC PKCS#11" = "${pkgs.opensc}/lib/opensc-pkcs11.so";
-            "DNIeRemote" = "${config.programs.dnieremote.finalPackage}/lib/libdnieremotepkcs11.so";
-          };
-        };
-
-        # Enable PC/SC smart card service
-        services.pcscd.enable = true;
-      })
-
-      ########################
-      # Hyprland Configuration
-      ########################
-      # Execute specific configuration module for this profile
-      ./ironman/desktop/hyprland/configuration.nix
-      # Execute configuration module for hyprland
-      ../modules/desktop/hyprland.nix
-
-      ########################
-      # Plasma 6 Configuration
-      ########################
-      # Execute specific configuration module for this profile
-      #./ironman/desktop/plasma/configuration.nix
-      # Execute configuration module for plasma
-      #../modules/desktop/plasma.nix
- 
-      # Execute home manager module
-      home-manager.nixosModules.home-manager {
-        home-manager.backupFileExtension = "backup";
-        home-manager.useGlobalPkgs = true;
-        home-manager.useUserPackages = true;
-        home-manager.extraSpecialArgs = {
-          # inherit username hyprland-plugins;
-          inherit username;
-          host = {
-            hostName = "ironman";
-          };
-        };
-        home-manager.users.${username} = {
-          ########################
-          # Hyprland Configuration
-          ########################
-          #imports = [walker.homeManagerModules.default] ++ [(import ./home.nix)] ++ [(import ./ironman/desktop/hyprland/home.nix)];
-          imports = [(import ./home.nix)] ++ [(import ../home-manager/desktop/hyprland/home.nix)];
-
-          ########################
-          # Plasma 6 Configuration
-          ########################
-          #imports = [(import ./home.nix)];
-        };
-      }
-
-    ];
-
-  };
 
   # Next profile if it is necessary
 }
