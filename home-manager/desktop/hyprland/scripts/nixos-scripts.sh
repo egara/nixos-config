b4bbca4fb1f2221d7d41830a7b9b87c0cde4f497
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2025-08-30 09:32:22 +0200

    Adding icons to the NixOS scripts menu

--- home-manager/desktop/hyprland/scripts/nixos-scripts.sh
+++ home-manager/desktop/hyprland/scripts/nixos-scripts.sh
@@ -7,16 +7,17 @@
 # @email: eloy.garcia.pca@gmail.com
 # ------------------------------------------
 
-items="Update\nClean\nExtranet\nEclipse"
+items="󰚰\u00A0\u00A0\u00A0\u00A0Update\n\u00A0\u00A0\u00A0\u00A0Clean\n󰱦\u00A0\u00A0\u00A0\u00A0Extranet\n\u00A0\u00A0\u00A0\u00A0Eclipse"
+
 output=$(echo -e $items | walker --dmenu -H)
 
-if [[ "$output" == "Update" ]]; then
+if [[ "$output" == *"Update"* ]]; then
   kitty --hold sh -c "~/.config/hypr/nixos-update.sh"
-elif [[ "$output" == "Clean" ]]; then
+elif [[ "$output" == *"Clean"* ]]; then
   kitty --hold sh -c "~/.config/hypr/nixos-clean.sh"
-elif [[ "$output" == "Extranet" ]]; then
+elif [[ "$output" == *"Extranet"* ]]; then
   kitty --hold sh -c "~/scripts/nixos/extranet.sh"
-elif [[ "$output" == "Eclipse" ]]; then
+elif [[ "$output" == *"Eclipse"* ]]; then
   . ~/scripts/nixos/eclipse.sh
 else
   echo "Select an option"
