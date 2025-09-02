edfd005fb26502e2bf0b904f367353cb060b9101
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2025-09-02 09:45:05 +0200

    Adding new shortcut for Hibernate Development

--- home-manager/desktop/hyprland/scripts/nixos-scripts.sh
+++ home-manager/desktop/hyprland/scripts/nixos-scripts.sh
@@ -7,7 +7,7 @@
 # @email: eloy.garcia.pca@gmail.com
 # ------------------------------------------
 
-items="󰚰\u00A0\u00A0\u00A0\u00A0Update\n\u00A0\u00A0\u00A0\u00A0Clean\n󰱦\u00A0\u00A0\u00A0\u00A0Extranet\n\u00A0\u00A0\u00A0\u00A0Eclipse"
+items="󰚰\u00A0\u00A0\u00A0\u00A0Update\n\u00A0\u00A0\u00A0\u00A0Clean\n󰱦\u00A0\u00A0\u00A0\u00A0Extranet\n\u00A0\u00A0\u00A0\u00A0Eclipse\n\u00A0\u00A0\u00A0\u00A0Eclipse - Hibernate"
 
 output=$(echo -e $items | walker --dmenu -H)
 
@@ -19,6 +19,8 @@ elif [[ "$output" == *"Extranet"* ]]; then
   kitty --hold sh -c "~/scripts/nixos/extranet.sh"
 elif [[ "$output" == *"Eclipse"* ]]; then
   . ~/scripts/nixos/eclipse.sh
+elif [[ "$output" == *"Hibernate"* ]]; then
+  kitty --hold sh -c "distrobox enter arch -- ~/distrobox/arch/programs/eclipse-2023-12/eclipse"
 else
   echo "Select an option"
 fi
