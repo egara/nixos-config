14d6c9fd7ee253567559823b506226f923dcc048
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2026-02-17 09:40:57 +0100

    Adding more themes

--- hosts/default.nix
+++ hosts/default.nix
@@ -59,7 +59,7 @@ let
           config = lib.mkIf (host.desktop == "hyprland") (
             let
               themeMode = "dark";
-              themeScheme = "catppuccin-mocha";
+              themeScheme = "ubuntu";
             in
             {
               programs.sicos.hyprland.enable = true;
