340b2f8235cc439d941abaa1fb5c27f0373505ab
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2026-03-07 20:37:51 +0100

    Adding a nerd welcome animation

---
+++ home-manager/desktop/hyprland/scripts/welcome-animation.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# SicOS Welcome Animation
+# ------------------------------------------
+# @author: Eloy García Almadén
+# @email: eloy.garcia.pca@gmail.com
+# ------------------------------------------
+
+# Centering both canvas and text
+echo "WELCOME TO SICOS. LOADING CONFIGURATIONS..." | tte --frame-rate 120 --anchor-canvas c --anchor-text c --no-restore-cursor --no-eol print --print-speed 1
+sleep 2
