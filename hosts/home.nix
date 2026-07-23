b30cdfc851dee2a76e5a9efaab9575b2372797c7
Eloy García Almadén <eloy.garcia.pca@gmail.com>
2026-07-23 10:36:47 +0200

    Removing keepassxc settings via home manager

--- hosts/home.nix
+++ hosts/home.nix
@@ -508,10 +508,6 @@ in
   programs.keepassxc = {
     autostart = true;
     enable = true;
-    settings = {
-      # For available settings, see https://github.com/keepassxreboot/keepassxc/blob/develop/src/core/Config.cpp
-      FdoSecrets.Enabled = true; # Enable Secret Service Integration
-    };
   };
   xdg.autostart.enable = true; # Enable creation of XDG autostart entries.
 
